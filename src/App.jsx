c5eb9d7328d6aa087e2445e4421ef986e8628aa1
Octávio Mello <octaviomellocovredesouza@gmail.com>
2026-08-27 10:25:33 -0300

    feat: Criação dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-const App = () => {
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Problem from "./components/Problem";
+import HowItWorks from "./components/HowItWorks";
+import Perception from "./components/Perception";
+import Experience from "./components/Experience";
+import MVP from "./components/MVP";
+import Autonomy from "./components/Autonomy";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+function App() {
   return (
     <>
-    
+      <Header />
+      <Hero />
+      <Problem />
+      <HowItWorks />
+      <Perception />
+      <Experience />
+      <MVP />
+      <Autonomy />
+      <About />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
